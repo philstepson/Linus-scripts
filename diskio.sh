c847f5bb5e73b7ace85f164e1c0e10ee19464e80
Philip Stephenson <phil@phil-stephenson.com>
2021-08-15 08:22:47 -0500

    Update diskio.sh

--- diskio.sh
+++ diskio.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "+-----------------------------------------------+"
+echo "|         Edit dh -h Output           |"
+echo "+-----------------------------------------------+"
 
 #write the output of df-h into an array
 readarray -t array <<< "$(df -h)"
@@ -12,7 +15,3 @@ echo "${array[1]}"
 #print out whole array - for illustration only - uncomment the following line if you need it
 
 #printf -- "%s\n" "${array[@]}"
-
-
-
-
